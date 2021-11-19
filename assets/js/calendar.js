94b3fa94b196783433d5188b48df22ed0529b932
Euthyphro <lulu.cao.her@gmail.com>
2021-11-19 14:39:33 -0600

    update calendar to November

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,27 +1,37 @@
-//get holidays from API and add them to the calendar
-let url = "https://calendarific.com/api/v2/holidays?api_key=bb3af9b5b18b01debdb7d770723b84dee8c9e30d&country=US&year=2021&month=10&type=national";
-fetch(url)
-.then(function(response){
-    response.json().then(function(data) {
-        console.log(data);
-        let holidayFromAPI = data.response.holidays[0].date.iso;
-        let holidayDateFormated = moment(holidayFromAPI, "YYYY-MM-DD").format("MMDDYY");
-        console.log(holidayDateFormated);
-        let dateFromCalendar = document.getElementById("date" + holidayDateFormated);
-        let holidayName = document.createElement('p');
-        holidayName.innerHTML = data.response.holidays[0].name;
-        holidayName.classList.add("custom-td");
-        holidayName.style.fontSize = "11px";
-        dateFromCalendar.appendChild(holidayName);
-    });
-}).catch(function(error) {
-    console.log('Fetch Error:', error);
-});
+// // //get holidays from API and add them to the calendar
+// let url = "https://calendarific.com/api/v2/holidays?api_key=bb3af9b5b18b01debdb7d770723b84dee8c9e30d&country=US&year=2021&month=10&type=national";
+// fetch(url)
+// .then(function(response){
+//     response.json().then(function(data) {
+//         console.log(data);
+//         let holidayFromAPI = data.response.holidays[0].date.iso;
+//         let holidayDateFormated = moment(holidayFromAPI, "YYYY-MM-DD").format("MMDDYY");
+//         console.log(holidayDateFormated);
+//         let dateFromCalendar = document.getElementById("date" + holidayDateFormated);
+//         let holidayName = document.createElement('p');
+//         holidayName.innerHTML = data.response.holidays[0].name;
+//         holidayName.classList.add("custom-td");
+//         holidayName.style.fontSize = "11px";
+//         dateFromCalendar.appendChild(holidayName);
+//     });
+// }).catch(function(error) {
+//     console.log('Fetch Error:', error);
+// });
 
 
-// highlight current date
-let currentDateFromMoment = moment().format('MMDDYY');
-console.log(currentDateFromMoment);
-let currentDateFromCalendar = document.getElementById("date" + currentDateFromMoment);
-currentDateFromCalendar.classList.add("table-active");
+// // highlight current date
+// let currentDateFromMoment = moment().format('MMDDYY');
+// console.log(currentDateFromMoment);
+// let currentDateFromCalendar = document.getElementById("date" + currentDateFromMoment);
+// currentDateFromCalendar.classList.add("table-active");
+
+
+function printMonthYear() {
+    let currentMonthYear = moment().format("MMMM YYYY");
+    let monthyear = document.getElementById("monthyear");
+    monthyear.innerHTML = "";
+    monthyear.innerHTML = `<h2>${currentMonthYear}</h2>`;
+};
+
+printMonthYear();
 
