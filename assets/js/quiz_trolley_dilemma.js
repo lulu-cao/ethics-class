2b84e4225550a7a3a0ff298540a3655d6e2b32b2
Euthyphro <lulu.cao.her@gmail.com>
2021-12-06 21:53:37 -0600

    fixed bug in the displayQuestion function

--- assets/js/quiz_trolley_dilemma.js
+++ assets/js/quiz_trolley_dilemma.js
@@ -1,4 +1,4 @@
-//questions from questions_trolley_dilemma.js can be used here directly without creating variables or imports
+//questions from "questions_trolley_dilemma.js" can be used here directly without creating variables or imports
 let startBtn = document.getElementById('startBtn');
 let initContainer = document.getElementById('initContainer');
 let timeLeft = document.getElementById('timeLeft');
@@ -17,39 +17,48 @@ let correctOrWrong = document.getElementById("correctOrWrong");
 
 function displayQuestion() {
     questionTitle.innerHTML = currentQuestion.question;
-    currentQuestion.choices.forEach((choices, i) => {
-        choicesEl.innerHTML = "";
-        let choiceItem = document.createElement("button");
-        choiceItem.setAttribute("class", "btn");
+    choicesEl.innerHTML = "";
+    currentQuestion.choices.forEach(function(choices, i) {
+        let choiceItem = document.createElement("btn");
+        choiceItem.setAttribute("class", "button");
         choiceItem.setAttribute("value", choices);
+        console.log(choices);
+        console.log(i);
         choiceItem.innerHTML = i + 1 + ". " + choices;
-        choiceItem.onclick = checkAnswer();
+        choiceItem.addEventListener("click", checkAnswer());
         choicesEl.appendChild(choiceItem)
     });
+    // The bug is fixed for this function; questions and choices display normally for the first question set
 };
 
-function startTimer() {
-    let i = 120; 
+// function startTimer() {
+//     let i = 120; 
 
-    let timeInterval = setInterval(function() {
-        i--;
-        timeLeft.textContent = i;
-        if (i < 1) {
-            clearInterval(timeInterval);
-            quizEnd();
-        };
-    }, 1000); 
-};
+//     let timeInterval = setInterval(function() {
+//         i--;
+//         timeLeft.textContent = i;
+//         if (i < 1) {
+//             clearInterval(timeInterval);
+//             quizEnd();
+//         };
+//     }, 1000); 
+// };
 
 function checkAnswer() {
-    console.log(currentQuestion);
-    if (this.value === currentQuestion.answer) {
-        correctOrWrong.innerHTML = "Correct!";
-        j++;
-        currentScore.textContent = j;
-    } else {
-        correctOrWrong.innerHTML = "Wrong!"
+    // start here!!!!!!!!!!!!!
+    function displayEval(event) {
+        console.log(currentQuestion);
+        if (this.value === questions[currentQuestionIndex].answer) {
+            correctOrWrong.innerHTML = "Correct!";
+            j++;
+            currentScore.textContent = j;
+        } else if (this.value !== questions[currentQuestionIndex].answer) {
+            console.log(this.value);
+            correctOrWrong.innerHTML = "Wrong!"
+        };
     };
+
+    console.log(correctOrWrong.innerHTML);
 
     setTimeout(function() {
         correctOrWrong.innerHTML = ""
@@ -69,7 +78,7 @@ function checkAnswer() {
 function quizEnd() {
     eval.removeAttribute("class");
     eval.innerHTML = `Your current score is: ${j}. Please enter your name below. <br/><br/>
-    <div class="input-group mb-3 col-6 mx-auto w-50">
+    <div class="input-group mb-3 mx-auto w-50">
         <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2">
         <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a href="./score.html">Submit</a></button>
     </div>
@@ -80,6 +89,6 @@ function quizEnd() {
 startBtn.addEventListener('click', function() {
     initContainer.style.display = "none";
     displayQuestion();
-    startTimer();
+    // startTimer();
 }
 );
