16312457068aa42c75aaed4c7e480a42f0bbf89f
Euthyphro <lulu.cao.her@gmail.com>
2021-12-17 17:49:10 -0600

    update assignment page

--- assets/js/assignment.js
+++ assets/js/assignment.js
@@ -1,41 +1,53 @@
 // initial display of "current time" when the page loads
-var currentTime = moment().format("MMM. D YYYY [at] h:mm:ss a");
+let currentTime = moment().format("MMM. D YYYY [at] h:mm:ss a");
 $("#currentTime").text(currentTime);
 
 // "current time" progresses when the page displays
 setInterval(function() {
-    var currentTime = moment().format("MMM. D YYYY [at] h:mm:ss a");
+    let currentTime = moment().format("MMM. D YYYY [at] h:mm:ss a");
     $("#currentTime").text(currentTime);
 }, 1000);
 
-// add calendar or datepicker to the modal
+// add datepicker to the modal
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
 
 console.log($('#exampleFormControlTextarea1').text());
 
-// setting up the "add" button for the modal
-var addAssignmentModal = $('#staticBackdrop');
-var formToAddAssignment = $('#custom-form');
-// setting up variables for the value inputted in the form and trimming the whitespace before and after the strings in the value
-var newName = $('#formGroupExampleInput').val().trim();
-var newDescription = $('#exampleFormControlTextarea1').val().trim();
-var newType = $('#newType').val().trim();
-var newPoints = $('#newPoints').val().trim();
-var newDueDate = $('#datepicker').val().trim();
+// add form submission function for the modal
+let addAssignmentModal = $('#staticBackdrop');
+let formToAddAssignment = $('#custom-form');
+let newTitle = $('#formGroupExampleInput').val().trim();
+let newDescription = $('#exampleFormControlTextarea1').val().trim();
+let newType = $('#newType').val().trim();
+let newPoints = $('#newPoints').val().trim();
+let newDueDate = $('#datepicker').val().trim();
+let addBtn = document.getElementById("custom-btn");
 
 function addNewAssignment(name, description, type, points, dueDate) {    
-    let createRow = $('<tr>');
-    let createNewName = $('<td>').addClass('p-2').text(name);
-    let createNewDescription = $('<td>').addClass('p-2').text(description);
-    let createNewType = $('<td>').addClass('p-2').text(type);
-    let createNewPoints = $('<td>').addClass('p-2').text(points);
-    let createNewDueDate = $('<td>').addClass('p-2').text(dueDate);
-    let deleteBtn = $('<td>').addClass('p-2 delete-project-btn text-center').text('Delete');
+    let createRow = document.createElement("tr");
+    let createNewTitle = document.createElement("td");
+    createNewTitle.classList.add('p-2');
+    createNewTitle.innerHTML = name;
+    let createNewDescription = document.createElement("td");
+    createNewDescription.classList.add('p-2');
+    createNewDescription.innerHTML = description;
+    let createNewType = document.createElement("td");
+    createNewType.classList.add('p-2');
+    createNewType.innerHTML = type;
+    let createNewPoints = document.createElement("td");
+    createNewPoints.classList.add('p-2');
+    createNewPoints.innerHTML = points;
+    let createNewDueDate = document.createElement("td");
+    createNewDueDate.classList.add('p-2');
+    createNewDueDate.innerHTML = dueDate;
+    let deleteBtn = document.createElement("td");
+    deleteBtn.classList.add('p-2 delete-project-btn text-center');
+    deleteBtn.innerHTML = 'Delete';
 
     createRow.append(
-        createNewName, 
+        createNewTitle, 
         createNewDescription, 
         createNewType, 
         createNewPoints,
@@ -48,10 +60,10 @@ function addNewAssignment(name, description, type, points, dueDate) {
     addAssignmentModal.modal('hide');
 };
 
-function formSubmit(event) {
+addBtn.addEventListener("click", function formSubmit(event) {
     event.preventDefault();
-    addNewAssignment(newName, newDescription, newType, newPoints, newDueDate);
-};
+    addNewAssignment(newTitle, newDescription, newType, newPoints, newDueDate);
+});
 
 function executeDeletion(event) {
     console.log(event.target);
@@ -59,11 +71,4 @@ function executeDeletion(event) {
     deleteConfirmed.parent('tr').remove();
 };
 
-// function a(){
-//     console.log("1")
-// };
-// $('#custom-btn').on('submit', a);
-
-//looks like something is wrong with the following line; as indicated by the above test (console couldn't log 1)
-formToAddAssignment.on('submit', formSubmit);
 $('#hwtable').on('click', '.delete-project-btn', executeDeletion);
