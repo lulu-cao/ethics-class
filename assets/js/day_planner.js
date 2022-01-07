6054a8589c00115a60c5557aaf69cc5c717b89fa
Euthyphro <lulu.cao.her@gmail.com>
2022-01-06 22:37:10 -0600

    color the rows in day planner based on time

--- assets/js/day_planner.js
+++ assets/js/day_planner.js
@@ -1,20 +1,46 @@
+// Display different rows with different colors based on if it is in the past or in the present/future
 let inputArray = document.getElementsByClassName("form-control");
 let btnArray = document.getElementsByClassName("btn");
 
+let currentTime = moment().format("k");
+console.log(currentTime);
+
+function colorRow() {
+  for (let j = 7; j < 21; j++) {
+    if (currentTime > j) {
+      console.log(j);
+      let string = `#row${j}`;
+      console.log(string);
+      console.log(document.querySelector(string));
+      document
+        .getElementsByClassName("custom-row")
+        [j - 7].classList.add("gray");
+      inputArray[j - 7].classList.add("lightgray");
+    } else {
+      document
+        .getElementsByClassName("custom-row")
+        [j - 7].classList.add("green");
+    }
+  }
+}
+
+colorRow();
+
 function init() {
-    for (let i=0; i<inputArray.length; i++) {
-        console.log("initiated");
-        btnArray[i].addEventListener("click", function storeData(event) {
-            event.preventDefault();
-            console.log("clicked");
-            let inputValue = inputArray[i].value.trim();
-            let storedData = JSON.parse(localStorage.getItem(inputValue)) || "";
-            if (!storedData) {
-                localStorage.setItem("inputValue", inputValue);
-            } else {
-                inputArray[i].innerHTML = storedData;
-            }
-        });
-}};
+  for (let i = 0; i < inputArray.length; i++) {
+    console.log("initiated");
+    btnArray[i].addEventListener("click", function storeData(event) {
+      event.preventDefault();
+      console.log("clicked");
+      let inputValue = inputArray[i].value.trim();
+      let storedData = JSON.parse(localStorage.getItem(inputValue)) || "";
+      if (!storedData) {
+        localStorage.setItem("inputValue", inputValue);
+      } else {
+        inputArray[i].innerHTML = storedData;
+      }
+    });
+  }
+}
 
 init();
