d43b6df0ed94cd0984784a7bf9a01a3bd8f7102b
Euthyphro <lulu.cao.her@gmail.com>
2022-01-11 22:42:48 -0600

    colorRow function is finished

--- assets/js/day_planner.js
+++ assets/js/day_planner.js
@@ -2,24 +2,15 @@
 let inputArray = document.getElementsByClassName("form-control");
 let btnArray = document.getElementsByClassName("btn");
 
-let currentTime = moment().format("k");
-console.log(currentTime);
+let currentTime = moment().format("k") - 8;
 
 function colorRow() {
   for (let j = 7; j < 21; j++) {
     if (currentTime > j) {
-      console.log(j);
-      let string = `#row${j}`;
-      // console.log(string);
-      // console.log(document.querySelector(string));
       document
         .getElementsByClassName("custom-row")
         [j - 7].classList.add("gray");
-      inputArray[j - 7].classList.add("lightgray");
-    } else {
-      document
-        .getElementsByClassName("custom-row")
-        [j - 7].classList.add("green");
+      inputArray[j - 7].classList.add("gray");
     }
   }
 }
